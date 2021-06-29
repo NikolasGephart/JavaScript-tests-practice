2dbac7fa80dd3dd798b9443d8e8ed51281284f43
ChyaBoiNik <nikolas.gephart@gmail.com>
2021-06-29 11:52:30 -0600

    gotta push

--- app/bonuses.js
+++ app/bonuses.js
@@ -1,6 +1,9 @@
 //Given the string "team number 2", transform and return the string in camelCase
 let str = "team number 2"
+var camel = camelCase()
 
 function camelCase(str) {
-
+  return camelCase
 }
+
+console.log(camel)
